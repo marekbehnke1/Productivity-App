5ac0aa6640a354ba51f94ae128e917e0efd87668
Marek <marek_ski@hotmail.co.uk>
2025-06-18 11:32:34 +0100

    collapsing items look nice now

---
+++ Converters/CollapsedHeightConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Avalonia.Svg.Skia;
+using LearnAvalonia;
+using LearnAvalonia.Models;
+
+namespace LearnAvalonia.Converters
+{
+    public class CollapsedHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is true)
+            {
+                return 250;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
